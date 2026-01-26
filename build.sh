9df517639015f54043ae6312fcef67fa5991b51f
control-o <ctrl.o.ctrl.i@gmail.com>
2026-01-26 21:44:10 +0100

    fix build for different environments

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-ol main.scm -x c | gcc -x c -o main -
+#!/usr/bin/env bash
+
+case "$USER" in
+  ctrl_o)    OWL=/home/krzysckh/bin/ol ;;
+  william)   OWL=~/Programming/owl-real/result/bin/ol ;;
+  *)         OWL=ol ;;
+esac
+$OWL main.scm -x c | gcc -x c -o main -
